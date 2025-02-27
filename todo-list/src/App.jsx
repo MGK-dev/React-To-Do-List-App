7d20a958db031346c93c4a5fad601df475930398
unknown <khannsaab01@gmail.com>
2025-02-27 21:02:11 +0500

    Install Tailwindcss with vite in the project, and test it for confirming that it is installed properly.

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div className="flex justify-center items-center h-screen bg-blue-500 text-red-800 text-2xl">
+        Tailwind CSS is Working! 🎉
+      </div>
+    );
+  }
+  
+  export default App;
+  
